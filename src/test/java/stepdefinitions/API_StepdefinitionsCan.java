e7a869d0331f5291a6e727e99512debad8b9768e
Can Yilmaz <canyilmaz9999@gmail.com>
2024-12-04 12:13:03 +0100

    Can/04.12.2024/BloodGroup

---
+++ src/test/java/stepdefinitions/API_StepdefinitionsCan.java
@@ -0,0 +1,17 @@
+package stepdefinitions;
+import base.BaseTest;
+import io.cucumber.java.en.Given;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import utilities.API_Utilities.API_Methods;
+
+import static hooks.HooksAPI.spec;
+import static io.restassured.RestAssured.given;
+import static utilities.API_Utilities.API_Methods.fullPath;
+
+public class API_StepdefinitionsCan extends BaseTest {
+
+    String exceptionMesaj = null;
+
+
+}
