e402bd39e42023eca6827134d5de18e1203d70da
Ismail KILGOZ <ligerhyme@hotmail.com>
2024-12-05 15:31:39 +0300

    "Mustafa/04.12/"

--- src/test/java/stepdefinitions/API_StepsMustafa.java
+++ src/test/java/stepdefinitions/API_StepsMustafa.java
@@ -3,6 +3,7 @@ package stepdefinitions;
 import base.BaseTest;
 import io.cucumber.java.en.Given;
 import org.hamcrest.Matchers;
+import pojos.Pojo;
 import utilities.API_Utilities.TestData;
 
 import static io.restassured.RestAssured.given;
@@ -59,5 +60,19 @@ public class API_StepsMustafa extends BaseTest {
         map = testData.expenseHeadUpdateRequestBody(id, exp_category, description, is_active, is_deleted);
 
         System.out.println("Patch Body : " + map);
+    }
+
+    @Given("The api user prepares a PATCH request that does not contain an id but includes {string}, {string}, {string} and {string} information to send to the api updateExpenseHead endpoint.")
+    public void the_api_user_prepares_a_patch_request_that_does_not_contain_an_id_but_includes_and_information_to_send_to_the_api_update_expense_head_endpoint(String string, String string2, String string3, String string4) {
+
+        // pojo
+    }
+
+
+    @Given("The api user prepares a GET request containing the {int} information to send to the api updateExpenseHead endpoint.")
+    public void the_api_user_prepares_a_get_request_containing_the_information_to_send_to_the_api_update_expense_head_endpoint(int id) {
+        requestBody.put("id", id);
+
+        System.out.println("Get Body : " + requestBody);
     }
 }
