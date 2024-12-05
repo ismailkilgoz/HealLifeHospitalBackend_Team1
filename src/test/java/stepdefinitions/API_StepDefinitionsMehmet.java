8057ecb99a13a9cbe695f2ab8af876588537e4bf
mehmetmuavviz <muavvizmir@gmail.com>
2024-12-05 20:09:45 +0100

    mehmet/05.12.24/day-end

--- src/test/java/stepdefinitions/API_StepDefinitionsMehmet.java
+++ src/test/java/stepdefinitions/API_StepDefinitionsMehmet.java
@@ -69,9 +69,43 @@ public class API_StepDefinitionsMehmet extends BaseTest {
 
         //response.prettyPrint();
     }
+    @Then("The Api User prepares a GET request containing the {int} information to send to the api getFindingById endpoint.")
+    public void the_api_user_prepares_a_get_request_containing_the_id_information_to_send_to_the_api_get_finding_by_id_endpoint(Integer id) {
+        requestBody.put("id", id);
 
+        System.out.println("Get Body : " + requestBody);
+    }
+    @Then("The Api User sends a GET body and saves the returned response.")
+    public void the_api_user_sends_a_get_body_and_saves_the_returned_response() {
+        response=given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .when()
+                .body(requestBody.toString())
+                .get(fullPath);
+    }
+    @Then("The Api User sends a GET body and saves the returned response body.")
+    public void the_api_user_sends_a_get_body_and_saves_the_returned_response_body() {
+        response=given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .when()
+                .body(map)
+                .get(fullPath);
+    }
+    @Then("The Api User verifies the information in the response body for the entry with the specified {int}, including {string}, {string}, {string}, {string} and {string}  .")
+    public void the_api_user_verifies_the_information_in_the_response_body_for_the_entry_with_the_specified_including_and(Integer id, String name, String description, String finding_category_id, String created_at, String category) {
+        repJP = response.jsonPath();
 
-
+        Assert.assertEquals(map.get("id"), repJP.getString("id"));
+        Assert.assertEquals(map.get("name"), repJP.getString("name"));
+        Assert.assertEquals(map.get("description"), repJP.getString("description"));
+        Assert.assertEquals(map.get("finding_category_id"), repJP.getString("finding_category_id"));
+        Assert.assertEquals(map.get("created_at"), repJP.getString("created_at"));
+        Assert.assertEquals(map.get("category"), repJP.getString("category"));
+    }
+    @Then("The Api User prepares a GET request that does not contain data")
+    public void the_api_user_prepares_a_get_request_that_does_not_contain_data() {    }
 
 
 
