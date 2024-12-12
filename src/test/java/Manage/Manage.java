60dec6fb84343868bd0f76305e137f53b43e8896
Umit URETEN <uretenumit@gmail.com>
2024-12-12 11:21:49 +0300

    umit/12.12.20224

--- src/test/java/Manage/Manage.java
+++ src/test/java/Manage/Manage.java
@@ -12,4 +12,22 @@ public class Manage {
     String US07="SELECT * FROM u201212290_heallifeqa.bed WHERE created_at = '2023-05-04 06:41:17';";
     String US08="SELECT * FROM u201212290_heallifeqa.bed  WHERE bed_group_id = 4 AND is_active = 'no';";
     String US09="SELECT * FROM u201212290_heallifeqa.birth_report WHERE weight >= 2.5;";
+
+    String US026="SELECT known_allergies, created_at FROM patients where known_allergies= 'Fast food' order by created_at asc LIMIT 1";
+    String US027="select distinct account_title from staff where qualification= 'MS' and specialization= 'Neurology' order by account_title asc";
+    String US028="SELECT driver_name, driver_licence, manufacture_year FROM vehicles WHERE driver_name = 'bayram erguven' and driver_licence= 'b' ORDER BY manufacture_year ASC LIMIT 1";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
