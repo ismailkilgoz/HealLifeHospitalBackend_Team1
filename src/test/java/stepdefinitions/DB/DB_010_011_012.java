de4b2cef581c38cd12b10785a3cd93585fa1e4ef
XaverNajafova <nurixaver@gmail.com>
2024-12-12 16:38:12 +0400

    xaver 12.12.2024

--- src/test/java/stepdefinitions/DB/DB_010_011_012.java
+++ src/test/java/stepdefinitions/DB/DB_010_011_012.java
@@ -5,6 +5,7 @@ import Manage.Manage_010_011_012;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import utilities.DB_Utilities.JDBCMethods;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -13,44 +14,60 @@ import java.sql.Statement;
 
 import static HelperDB.JDBC_Structure_Methods.*;
 import static HelperDB.JDBC_Structure_Methods.resultSet;
+import static org.junit.Assert.assertEquals;
+import static utilities.DB_Utilities.JDBCMethods.executeQuery;
 
 public class DB_010_011_012 extends Manage_010_011_012 {
     CommonData data = new CommonData();
     Statement statement;
     ResultSet resultSet;
     ResultSetMetaData metaData;
+    JDBCMethods jdbcMethods = new JDBCMethods();
 
-    @Given("User creates Query for {string}")
-public void user_creates_query_for(String us) throws SQLException {
-        query=getDB_US010();
-        resultSet=getStatement().executeQuery(query);
-        try {
-            switch (us) {
-                case "DB_US010":
-                    query = getDB_US010();
-                    statement = getStatement();
-                    resultSet = statement.executeQuery(query);
-                    break;
 
-            }
-        } catch (Exception e) {
-            System.out.println("You entered the US name incorrectly or not at all");
-        }
+
+    @Then("The user verifies children who are siblings")
+    public void the_user_verifies_children_who_are_siblings() throws SQLException {
+        jdbcMethods.birthRecordAccess(resultSet);
     }
+    @When("I execute query the database for {string}")
+    public void i_execute_query_the_database_for(String queryId) throws SQLException {
+        Statement statement = connection.createStatement();
+        if (queryId.equals("DB_US10")) {
+            String query = "SELECT child_name, case_reference_id FROM birth_report WHERE case_reference_id IN (SELECT case_reference_id FROM birth_report GROUP BY case_reference_id HAVING COUNT(*) > 1);";
 
-    public String getDB_US010() {
-        return "";
-    }
-
-
-    @Given("User prints the ‘DB_US010’ query response returned")
-        public void user_prints_the_db_us010_query_response_returned () throws SQLException {
-        try {
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("child_name"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+            resultSet = statement.executeQuery(query);
         }}
+        @When("Execute query the database for {string}.")
+        public void execute_query_the_database_for(String queryId) throws SQLException {
+            Statement statement = connection.createStatement();
+            if (queryId.equals("DB_US11")) {
+                String query = "SELECT id, donor_name, date_of_birth, gender, father_name, address FROM blood_donor WHERE id = 7;";
+                resultSet = statement.executeQuery(query);
+            }
+    }
+        @Then("The user validates donor data")
+        public void the_user_validates_donor_data () throws SQLException {
+            resultSet.next();
+            assertEquals(resultSet.getInt("id"), 7);
+            assertEquals(resultSet.getString("donor_name"), "Maria");
+            assertEquals(resultSet.getString("date_of_birth"), "2001-03-02");
+            assertEquals(resultSet.getString("gender"), "Female");
+            assertEquals(resultSet.getString("father_name"), "Jhonson");
+        }
+        @When("I execute query the  database  foor {string}")
+    public void i_execute_query_the_database_foor(String queryId) throws SQLException {
+            Statement statement = connection.createStatement();
+            if (queryId.equals("DB_US12")) {
+                String query = "SELECT * FROM blood_donor_cycle WHERE institution IS NOT NULL AND id = 17;";
+                resultSet = statement.executeQuery(query);
+            }}
+            @Then("The user finds id 17 in the listed blood_donor_cycle table information")
+            public void the_user_finds_id_in_the_listed_blood_donor_cycle_table_information () throws SQLException {
+                resultSet.next();
+                assertEquals(resultSet.getInt("id"), 17);
 
+
+            }
+
+        }
--- target/test-classes/features/db/DB_010_011_012.feature
+++ target/test-classes/features/db/DB_010_011_012.feature
@@ -1,30 +1,26 @@
-Feature:
+Feature: This feature is a file created for Database User Stories 10,11 12
+
+  Background:
+    Given Database connection established
+
+  Scenario: DB_US10: List and verify the names of babies who are siblings in the birth_report table.
+
+    When I execute query the database for "DB_US10"
+    Then The user verifies children who are siblings
+    And Database closed
+
+  Scenario: DB_US11: Verify that the blood donor with id = 7 has donor_name = Maria, date_of_birth = '2001-03-02', gender = Female, father_name = Jhonson, address = England in the blood_donor table.
+
+    When Execute query the database for "DB_US11".
+    Then The user validates donor data
+    And Database closed
+
+  Scenario: DB_US12: List the id numbers of blood_donor_cycle entries where the institution is known, and verify that id = 17 is in the list.
+
+    When I execute query the  database  foor "DB_US12"
+    Then The user finds id 17 in the listed blood_donor_cycle table information
+    And Database closed
 
 
-Scenario: DB_US010 List and verify the names of babies who are siblings in the birth_report table.
-* Database connection established
-* User creates Query for "DB_US010"
-* User prints the ‘DB_US010’ query response returned
-* Database closed
 
-
-
-
-Scenario:  DB_US11 Verify that the blood donor with id = 7 has donor_name = Maria, date_of_birth = '2001-03-02', gender = Female, father_name = Jhonson, address = England in the blood_donor table.
-   Given Database connection established
-    Given a blood_donor table with donor details
-    When I query for the donor with id = 7
-    Then the donor_name should be "Maria"
-    And the date_of_birth should be "2001-03-02"
-    And the gender should be "Female"
-    And the father_name should be "Jhonson"
-    And the address should be "England"
-    Then Database closed
-
-Scenario: List the id numbers of blood_donor_cycle entries where the institution is known, and verify that id = 17 is in the list.
-* Database connection established
-* Creates query with
-* Creates query with
-* Verifies that it CONTAINS datas : "id" values : "17" message : "false"
-* Database closed
 
